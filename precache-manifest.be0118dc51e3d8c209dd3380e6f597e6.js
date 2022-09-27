2a2e7cd1250648267579e663a394c77fcaf79733
KeerthiNarayana <keerthinarayanaprasad2009@gmail.com>
2022-09-27 22:40:33 +0530

    Updates

---
+++ precache-manifest.be0118dc51e3d8c209dd3380e6f597e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5acab1503d022bf27e30bd3cccd12af9",
+    "url": "/Keerthi2009/clear-cache-react-app-master.git/index.html"
+  },
+  {
+    "revision": "c5a9ebf0ec4dac1d6390",
+    "url": "/Keerthi2009/clear-cache-react-app-master.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e494b309aca42f828599",
+    "url": "/Keerthi2009/clear-cache-react-app-master.git/static/js/2.8b06242c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Keerthi2009/clear-cache-react-app-master.git/static/js/2.8b06242c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a9ebf0ec4dac1d6390",
+    "url": "/Keerthi2009/clear-cache-react-app-master.git/static/js/main.ee119241.chunk.js"
+  },
+  {
+    "revision": "6c26fa714bcd4e7171a7",
+    "url": "/Keerthi2009/clear-cache-react-app-master.git/static/js/runtime-main.b7c327a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Keerthi2009/clear-cache-react-app-master.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
